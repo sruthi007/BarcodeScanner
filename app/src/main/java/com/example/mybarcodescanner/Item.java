ad78d07c6a199bfe493eb2a6ef4e013ed49f7131
sruthi007 <sruthi38.ram@gmail.com>
2024-06-11 20:57:07 +0200

    First working commit

--- app/src/main/java/com/example/mybarcodescanner/Item.java
+++ app/src/main/java/com/example/mybarcodescanner/Item.java
@@ -3,14 +3,18 @@ package com.example.mybarcodescanner;
 public class Item {
     private String barcode;
     private String name;
+    private String mfdDate;
+    private String expiryDate;
 
     public Item() {
         // Default constructor required for calls to DataSnapshot.getValue(Item.class)
     }
 
-    public Item(String barcode, String name) {
+    public Item(String barcode, String name, String mfdDate, String expiryDate) {
         this.barcode = barcode;
         this.name = name;
+        this.mfdDate = mfdDate;
+        this.expiryDate = expiryDate;
     }
 
     public String getBarcode() {
@@ -27,5 +31,21 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getMfdDate() {
+        return mfdDate;
+    }
+
+    public void setMfdDate(String mfdDate) {
+        this.mfdDate = mfdDate;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
     }
 }
